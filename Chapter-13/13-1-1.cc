22b7e1ce1984f2d185fd4bca7f3a34f2d8958ef9
chenxian <chenxian_0@163.com>
2016-03-02 16:26:02 +0800

    Delete & Insert & Sort -- List

--- Chapter-13/13-1-1.cc
+++ Chapter-13/13-1-1.cc
@@ -63,10 +63,101 @@ void Print(node *head)
 	}
 }
 
+//Delete Num In List
+node* Delete(node *head, int num)
+{
+	node *p1, *p2;
+	p1 = head;
+	while(p1->data != num && p1->next != NULL)
+	{
+		p2 = p1;
+		p1 = p1->next;
+	}
+	if(p1->data == num)
+	{
+		if(p1 == head)
+		{
+			head = p1->next;
+		}
+		else
+		{
+			p2->next = p1->next;
+		}
+		free(p1);
+		p1 = NULL;
+	}
+	else
+		cout << "Not Found Num" << endl;
+	return head;
+}
+
+//Insert Num To List
+node* Insert(node *head, int num)
+{
+	node *p1, *p2;
+	p1 = head;
+	node *p0 = (node*)malloc(sizeof(node));
+	p0->data = num;
+	while(p1->data < p0->data && p1->next != NULL)
+	{
+		p2 = p1;
+		p1 = p1->next;
+	}
+	if(p1->data >= p0->data)
+	{
+		if(p1 == head)
+		{	
+			p0->next = p1;
+			head = p0;
+		}
+		else
+		{
+			p0->next = p1;
+			p2->next = p0;
+		}
+	}
+	else
+	{
+		p1->next = p0;
+		p0->next = NULL;
+	}
+	return head;
+}
+
+//Sort List
+node* Sort(node *head)
+{
+	node *p1, *p2;
+	int i, j, temp;
+	if(head == NULL || head->next == NULL)
+		return head;
+	int len = Length(head);
+	for(j=1; j<len; ++j)
+	{
+		p1 = head;
+		for(i=0; i<len-j; ++i)
+		{
+			if(p1->data > p1->next->data)
+			{
+				temp = p1->data;
+				p1->data = p1->next->data;
+				p1->next->data = temp;
+			}
+			p1 = p1->next;
+		}
+	}
+	return head;
+}
+
 int main()
 {
 	node *head = Create();
 	cout << Length(head) << endl;
+	Print(head);
+	head = Delete(head, 4);
+	head = Insert(head, 7);
+	Print(head);
+	head = Sort(head);
 	Print(head);
 	return 0;
 }
